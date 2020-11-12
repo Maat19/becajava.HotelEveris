e984bf09ff95c751bad0f13a603ed8893d1d137c
Mateus Maia Agre <mmaiaagr@everis.com>
2020-11-12 14:27:56 -0300

    Criacao Comodidade Controller

--- src/main/java/br/hoteleveris/app/controller/ComodidadeController.java
+++ src/main/java/br/hoteleveris/app/controller/ComodidadeController.java
@@ -8,42 +8,42 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.hoteleveris.app.request.ClienteRequest;
+import br.hoteleveris.app.request.ComodidadeRequest;
 import br.hoteleveris.app.response.BaseResponse;
-import br.hoteleveris.app.service.ClienteService;
+import br.hoteleveris.app.service.ComodidadeService;
 
 @RestController
-@RequestMapping("/clientes")
-public class ClienteController extends BaseController {
+@RequestMapping("/comodidades")
+public class ComodidadeController extends BaseController {
 
-	private final ClienteService _service;
+	private final ComodidadeService _service;
 
-	public ClienteController(ClienteService service) {
+	public ComodidadeController(ComodidadeService service) {
 		_service = service;
 	}
 
 	@PostMapping
-	public ResponseEntity<BaseResponse> criar(@RequestBody ClienteRequest request) {
+	public ResponseEntity<BaseResponse> inserir(@RequestBody ComodidadeRequest request) {
 		try {
-			BaseResponse response = _service.criar(request);
+			BaseResponse response = _service.inserir(request);
 			return ResponseEntity.status(response.statusCode).body(response);
+			
 		} catch (Exception e) {
 			return ResponseEntity.status(error.statusCode).body(error);
-		}
 
+		}
 	}
+
 	
-	
-	@GetMapping(path="/{id}") 
-	public ResponseEntity  obter(@PathVariable Long id){
+	@GetMapping (path ="/{id}")
+	public ResponseEntity obter (@PathVariable Long id) {
 		try {
 			BaseResponse response = _service.obter(id);
 			return ResponseEntity.status(response.statusCode).body(response);
 		} catch (Exception e) {
 			return ResponseEntity.status(error.statusCode).body(error);
 		}
-		
-		
 	}
-
+		
+	
 }
